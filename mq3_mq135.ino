9495056cbdc34e45a76903a00ae97781718646dd
Santiago Aghemo <106884348+santiago-aghemo@users.noreply.github.com>
2024-10-16 17:51:46 -0300

    Update mq3_mq135.ino
    comentarios

--- mq3_mq135.ino
+++ mq3_mq135.ino
@@ -13,8 +13,8 @@ void loop()
   float value_adc = raw_adc * (5.0 / 1023.0);
 
   Serial.print("PPM:  ");
-  Serial.print(raw_adc);
-  Serial.print("    Tension: ");
+  Serial.print(raw_adc, DEC);
+  Serial.print("    Tension: ");//poder ver la tension esta bueno porque con eso podemos ver si esta calentando el sensor o no. (Si lo acabamos de prender se empezara a calentar con una tension de  5V y lentamente va bajando hasta llegar al destado de reposo con una  tension  >1V) 
   Serial.println(value_adc);
 
   delay(MQ_DELAY);
